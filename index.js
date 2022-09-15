7de0b525ec106fc1476dff8c8fab04fe418b4712
Daiane <99364026+DaianedeOliveira@users.noreply.github.com>
2022-09-15 10:16:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 function geoFindMe() {
 
     const status = document.querySelector('#status');
     const mapLink = document.querySelector('#map-link');
+    
 
 
     mapLink.href = '';
@@ -13,24 +15,24 @@ function geoFindMe() {
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
 
+
     status.textContent = '';
     mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
     mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
   }
 
   function error() {
-    status.textContent = 'Unable to retrieve your location';
+    status.textContent = 'Não é possivel localizá-lo';
   }
 
   if (!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
+    status.textContent = 'Geolocalização não é suportando em seu navegador';
   } else {
-    status.textContent = 'Locating…';
+    status.textContent = 'Carregando…';
     navigator.geolocation.getCurrentPosition(success, error);
   }
 
 }
 
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
-
+document.querySelector('#find').addEventListener('click', geoFindMe);
 
